7d69c3c00cb3dc2f4641edcebcdb5bbb2466f3b7
John <jmitropoulos@alumni.berklee.edu>
2022-03-06 14:58:32 -0600

    Permissions logic implemented in main Activity

--- app/src/main/java/android/example/calllogcontentprovider/MainActivity.kt
+++ app/src/main/java/android/example/calllogcontentprovider/MainActivity.kt
@@ -1,11 +1,33 @@
 package android.example.calllogcontentprovider
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, Array(1) {Manifest.permission.READ_CALL_LOG}, 101)
+        } else
+            displayLog()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode === 101 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            displayLog()
+    }
+
+    private fun displayLog() {
+        TODO("Not yet implemented")
     }
 }
